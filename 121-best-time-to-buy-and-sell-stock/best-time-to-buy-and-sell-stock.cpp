cd9ec7f8f7ccaa160c43237f1d463b74829d5334
Abdul Rahman <136063204+abdulrahman2202@users.noreply.github.com>
2026-07-27 17:01:55 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (61.08%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -6,11 +6,15 @@ public:
 
         for(int i = 0;i<prices.size();i++){
 
-            //
+            // checking the lowest buy price and saving in the minPrice Variable
             if(prices[i]<minPrice){
                 minPrice = prices[i];
             }
+
+            // finding the difference(profit) from Buy price and selling price
             int profit = prices[i] - minPrice;
+
+            // if find greatest profit then change the maxProfit 
             if(profit > maxProfit){
                 maxProfit = profit;
             }

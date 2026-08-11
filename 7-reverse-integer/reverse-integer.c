bb5c596f427ee2dc4b2e4146ef7a6f13e7971c37
Abdul Rahman <136063204+abdulrahman2202@users.noreply.github.com>
2026-08-11 18:09:11 +0530

    Time: 3 ms (55.63%) | Memory: 9.2 MB (86.22%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,13 @@
+int reverse(int x){
+    long long reverse = 0;
+    while(x!=0){
+        reverse = (reverse * 10) + (x%10);
+        x = x/10;
+    }
+
+    if(reverse> INT_MAX || reverse< INT_MIN){
+        return 0;
+    }
+
+    return reverse;
+}

6b3a82c699bfa9ec3a1e72e0c74f1d353de1c2d0
Abdul Rahman <136063204+abdulrahman2202@users.noreply.github.com>
2026-09-23 18:48:26 +0530

    Time: 1 ms (52.06%) | Memory: 9 MB (21.68%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,18 @@
+bool isPalindrome(int x) {
+    if(x<0){
+        return false;
+    }
+    long long reverse = 0;
+    int temp = x;
+    while(temp!=0){
+        reverse = (reverse * 10) + (temp % 10);
+        temp = temp / 10;
+    }
+
+    if(reverse == x){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
